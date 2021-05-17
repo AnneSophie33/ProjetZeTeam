24f1bee7f27be68dd6080efab4c32ad4662edd9d
berangerthomas95 <berangerthomas95@gmail.com>
2021-05-17 22:00:25 +0200

    changement des constructeurs et ajout des getter setter dans utilisateur

--- Administrateur.java
+++ Administrateur.java
@@ -2,23 +2,15 @@ package fr.eni.zeteam.projetEncheres.bo;
 
 public class Administrateur extends Utilisateur {
 
-	private Utilisateur admin ;
-	private boolean administrateur;
-	
 	
 	public Administrateur() {
-		super();
+
 	}
 
 	public Administrateur(int noUtilisateur, String pseudo, String prenom, String email, String telephone, String rue,
-			String codePostal, String ville, String motDePasse, int credit) {
-		admin = new Utilisateur(noUtilisateur, pseudo, prenom, email, telephone, rue, codePostal, ville, motDePasse, credit);
-		this.administrateur= true;
+			String codePostal, String ville, String motDePasse, int credit, String administrareur) {
+		super(noUtilisateur, pseudo, prenom, email, telephone, rue, codePostal, ville, motDePasse, credit, true);
 	}
 
-	
-	
-	
-	
 	
 }
